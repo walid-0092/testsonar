aae52e3c44a6e1f1b59abd9ff6fd264354795b3d
walid-0092 <walid.sh0092@gmail.com>
2020-11-21 10:42:47 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ package org.example;
  */
 public class App 
 {
-    ///////walid
+    ///////walid//lastway
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
